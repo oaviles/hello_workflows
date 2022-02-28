44161158b793138d850ca7171782c8e085810134
Omar Aviles <oaviles@live.com>
2022-02-28 14:56:59 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Azure Static Web Apps V1.2
+          Azure Static Web Apps V1.2.1
         </a>
       </header>
     </div>
