7ab357dd9b20ce7075edba42434c4cf0194e4927
Omar Aviles <oaviles@live.com>
2023-03-07 07:53:23 -0600

    1.2.3.0

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Azure Static Web Apps V1.2.3
+          Azure Static Web Apps V1.2.3.0
         </a>
       </header>
     </div>
